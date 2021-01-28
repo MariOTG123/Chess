952a39a3542f6837a6bc74bc74555a5f503b6439
hej <hej@hej.com>
2021-01-28 09:21:07 +0100

    Added comments

--- Chess.py
+++ Chess.py
@@ -20,7 +20,9 @@ x = 75
 y = 75
 
 def imageb(x,y):
-    screen.blit(gpb,(x,y))
+    screen.blit(gpb,(int(x),int(y)))
+def imagew(x,y):
+    screen.blit(gpw,(int(x),int(y)))
 
 black = (0,0,0)
 white = (255,255,255)
@@ -37,6 +39,7 @@ def movableImg():
     mouse = pg.mouse.get_pos()
     click = pg.mouse.get_pressed()
     imageb(x,y)
+    imagew(x,y)
     if click[0] == 0:
         drag = 0
     if click[0] == 1 and x + l > mouse[0] > x and y + l > mouse[1] > y:
@@ -49,25 +52,29 @@ def movableImg():
 
 def main():
     while True:
-
+        # Quit Game
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
+        # Mouse Coordinates Detection
         if event.type == pg.MOUSEBUTTONDOWN:
             pos = pg.mouse.get_pos()
             c = pos[0] // (l+2)
             r = pos[1] // (l+2)
-            print(pos,c,r)
+            #print(pos,c,r)
+        if event.type == pg.MOUSEBUTTONUP:
+            if k + l > x > k and k + l > y > k:
+                pass
 
         screen.fill(black)
-
+        # Draw Board
         for c in range(w):
             for r in range(h):
                 color = white
                 pg.draw.rect(screen, color, pg.Rect(c*l+m+k,r*l+m+k,l-m,l-m))
 
         # Draw Game Piece
-        #screen.blit(gpb,pg.Rect(startb[0]*l+m+k,startb[1]*l+m+k,l-m,l-m))
+        screen.blit(gpb,pg.Rect(0*l+m+k,7*l+m+k,l-m,l-m))
 
         movableImg()
 
